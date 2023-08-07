1a20e2402161e21ff3538d7e810ec22e85a75396
Hassan Hatamravesh <hhatamravesh@gmail.com>
2023-08-07 23:07:00 +0330

    Added homework 6

---
+++ homework_6_1402_05_07/ValidParentheses_UniqueValuesConsoleApp/ValidParentheses_UniqueValuesConsoleApp/Helper.cs
@@ -0,0 +1,110 @@
+﻿namespace ValidParentheses_UniqueValuesConsoleApp
+{
+    internal class Helper
+    {
+        /// <summary>
+        /// This method checks if a string has valid sets of parentheses or not.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsValid(string str)
+        {
+            return IsValid(str.ToCharArray());
+        }
+        /// <summary>
+        /// This method checks if an array of chars has valid sets of parentheses or not.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsValid(char[] str)
+        {
+            Stack<char> stack = new Stack<char>();
+            char[] parentheses = { '(', '[', '{', ')', ']','}'};
+
+            foreach (char character in str)
+            {
+                if(!parentheses.Contains(character))
+                    continue;
+                if(character == '{' || character == '[' || character == '(')
+                {
+                    stack.Push(character);
+                }
+                else
+                {
+                    if(!stack.Any())
+                    {
+                        return false;
+                    }
+                    char stackTop = stack.Peek();
+                    if((character == ')' && stackTop == '(') ||
+                       (character == '}' && stackTop == '{') ||
+                       (character == ']' && stackTop == '['))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        /// <summary>
+        /// This method takes an ascending-sorted array and returns number of unique values of it or 0 if array is empty.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static int CountUniqueItems(int[] numbers)
+        {
+            if(numbers.Length <= 0)
+            {
+                return 0;
+            }
+            int count = 1;
+            //int[] uniques = new int[numbers.Length];
+            //uniques[0] = numbers[0];
+            
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if(numbers[i] != numbers[i -1])
+                    count++;
+                    //uniques[++count] = numbers[i];
+            }
+
+            return count;
+        }
+
+        //public static int CountUniqueItems<T>(T[] numbers) where T : struct, IConvertible, IComparable
+        //public static int CountUniqueItems<T>(List<T> numbers) where T : struct, IConvertible, IComparable
+        //{
+        //    if(numbers.Count <= 0)
+        //    {
+        //        return -1;
+        //    }
+        //    int count = 1;
+        //    List<T> uniques = new List<T>(); 
+        //    //T[] uniques = new T[numbers.Count];
+        //    uniques.Add(numbers[0]);
+
+        //    for(int i = 0; i < numbers.Count; i++)
+        //    {
+        //        bool isUnique = true;
+        //        for(int j = 0; j <= i; j++)
+        //        {
+        //            T temp1 = uniques[j];
+        //            T temp2 = numbers[i];
+        //            if(temp1 == temp2)
+        //                isUnique = false;
+        //        }
+        //        if(isUnique)
+        //        {
+        //            uniques[++count] = numbers[i];
+        //        }
+        //    }
+
+        //    return count;
+        //}
+    }
+}
---
+++ homework_6_1402_05_07/ValidParentheses_UniqueValuesConsoleApp/ValidParentheses_UniqueValuesConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ValidParentheses_UniqueValuesConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+            Console.WriteLine(Helper.CountUniqueItems(numbers));
+
+            string chars = "hsdf(fei[(sdfmdsi)]sd)asd";
+            //char[] chars = { '[' , ')','(',']' };
+
+            Console.WriteLine(Helper.IsValid(chars));
+        }
+    }
+}
