b5154512ec1a7aa6a4dc7315c67e5471e6c5ad77
Hassan Hatamravesh <hhatamravesh@gmail.com>
2023-08-07 23:03:36 +0330

    Added homework 3

---
+++ homework_3_1402_04_24/CustomArrayConsoleApp/CustomArrayConsoleApp/CustomArray.cs
@@ -0,0 +1,75 @@
+﻿#region Usings
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+#endregion
+
+namespace CustomArrayConsoleApp
+{
+    internal class CustomArrayTemplate<T>
+    {
+
+    }
+
+    internal class CustomArray
+    {
+        static public int SecondMaxNumber(in int[] array)
+        {
+            if(array == null || array.Length == 0)
+                throw new ArgumentException("array has no elements");
+
+            if(array.Length > 1)
+            {
+                int maxElement = array[0];
+                int secondMaxElement = array[0];
+
+                for(int i = 1; i < array.Length; i++)
+                {
+
+
+                    if(array[i] > maxElement)
+                    {
+                        secondMaxElement = maxElement;
+                        maxElement = array[i];
+                    }
+                }
+                return secondMaxElement;
+
+            }
+
+            return array[0];
+        }
+
+        static public bool FindDuplicate(in int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                int element = array[i];
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if(element == array[j])
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        static public string LogString(in int[] array)
+        {
+            string output = "for array with elements of: {";
+            foreach(int element in array)
+            {
+                output += element;
+                output += ", ";
+            }
+            output = output.Remove(output.Length - 2);
+            output += "}";
+            return output;
+        }
+    }
+}
---
+++ homework_3_1402_04_24/CustomArrayConsoleApp/CustomArrayConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CustomArrayConsoleApp
+{
+    internal class Program
+    {
+        static public int ReverseNumber(int number)
+        {
+            int numberSign = 1;
+            if(number < 0)
+            {
+                numberSign = -1;
+                number *= -1;
+            }
+
+            int reversedNumber = 0;
+
+            while(number > 0)
+            {
+                int digit = number % 10;
+                reversedNumber = reversedNumber * 10 + digit;
+                number /= 10;
+            }
+
+            return reversedNumber * numberSign;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 28, 16, 75, 5, 1, 28};
+            int number = -65749;
+
+            int secondMaxNumber = CustomArray.SecondMaxNumber(array);
+            int reverseNumber = ReverseNumber(number);
+            bool containsDuplicate = CustomArray.FindDuplicate(array);
+            string logString = CustomArray.LogString(array);
+
+            Console.WriteLine(logString + " the second max element is: " + secondMaxNumber);
+            Console.WriteLine($"reverse of {number} is {reverseNumber}");
+            Console.WriteLine(logString + $" duplication is {containsDuplicate}");
+        }
+    }
+}
