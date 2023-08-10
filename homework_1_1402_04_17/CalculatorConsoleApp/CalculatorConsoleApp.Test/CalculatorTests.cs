31556685d3a8ebab2e76f04b35daeb3c09934692
Hassan Hatamravesh <hhatamravesh@gmail.com>
2023-08-10 12:17:56 +0330

    Added xunit for writing tests and wrote a simple test

---
+++ homework_1_1402_04_17/CalculatorConsoleApp/CalculatorConsoleApp.Test/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorConsoleApp;
+using Xunit;
+
+namespace CalculatorConsoleApp.Test
+{
+    public class CalculatorTests
+    {
+        [Fact] // 'Fact' decorator says that this is a test and I want you to run it in test runner
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            // Arrange:
+            double expected = 5;
+
+            // Act:
+            double actual = SimpleCalculator.Add(3, 2);
+
+            // Assert:
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ homework_1_1402_04_17/CalculatorConsoleApp/CalculatorConsoleApp/SimpleCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    public static class SimpleCalculator
+    {
+        public static double Add(double x, double y) 
+        {
+            return x + y;
+        }
+
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
