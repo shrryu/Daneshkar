80e9b4f8352fdd3d6bea2f39fd84ebb40777ba2f
Hassan Hatamravesh <hhatamravesh@gmail.com>
2023-08-26 08:55:58 +0330

    Added homework7 solutions

---
+++ homework_7_1402_05_31/Homework7ConsoleApp/Homework7ConsoleApp/Helper.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Homework7ConsoleApp
+{
+    internal class Helper
+    {
+        public static bool IsAnagram(string original, string changed)
+        {
+            if(original.Length != changed.Length)
+                return false;
+
+            string tempo = changed;
+
+            for (int i = 0; i < changed.Length;i++)
+            {
+                var originalCharacter = original[i];
+                var occurenceIndex = tempo.IndexOf(originalCharacter);
+                if(occurenceIndex != -1)
+                {
+                    tempo = tempo.Remove(occurenceIndex, 1);
+                }
+                else
+                {
+                    return false; 
+                }
+            }
+
+            return true;
+        }
+
+        public static int AddDigits(int num)
+        {
+            bool oneDigitFlag = false;
+            int sum = 0;
+            while(!oneDigitFlag)
+            {
+                sum += (num % 10);
+                num /= 10;
+                if(num == 0)
+                {
+                    if(sum /10 == 0)
+                    {
+                        oneDigitFlag = true;
+                    }
+                    else
+                    {
+                        num = sum;
+                        sum = 0;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public static bool IsUgly(int number)
+        {
+            if(number < 1)
+                return false;
+
+            while (number > 1)
+            {
+                if(number % 2 == 0)
+                {
+                    number /= 2;
+                }
+                else if(number % 3 == 0)
+                {
+                    number /= 3;
+                }
+                else if(number % 5 == 0)
+                {
+                    number /= 5;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // do it inplace, no copies.
+        public static void MoveZeroes(int[] nums)
+        {
+            int indexOfLastNonZero = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != 0)
+                {
+                    nums[indexOfLastNonZero++] = nums[i];  
+                }
+            }
+
+            for (;  indexOfLastNonZero < nums.Length; indexOfLastNonZero++)
+            {
+                nums[indexOfLastNonZero] = 0;
+            }
+
+        }
+        public static bool WordPattern(string pattern, string str)
+        {
+            string[] strSplitted = str.Split(' ');
+            if(strSplitted.Length != pattern.Length)
+                return false;
+
+            Dictionary<char, string> matchedCharactersWithWords = new ();
+
+            for(int i = 0; i < pattern.Length; i++)
+            {
+                if(!matchedCharactersWithWords.ContainsKey(pattern[i]))
+                {
+                    if(!matchedCharactersWithWords.ContainsValue(strSplitted[i]))
+                    {
+                        matchedCharactersWithWords.Add(pattern[i], strSplitted[i]);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if(matchedCharactersWithWords[pattern[i]] != strSplitted[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+
+    }
+}
---
+++ homework_7_1402_05_31/Homework7ConsoleApp/Homework7ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Homework7ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Valid Anagram:
+            string originalStr = "anagram";
+            string anagram = "maaagrn";
+            bool anagramResult = Helper.IsAnagram(originalStr, anagram);
+            Console.WriteLine($"is anagram? {anagramResult}");
+
+            /// Add Digits:
+            int num = 486;
+            int addDigitResult = Helper.AddDigits(num);
+            Console.WriteLine($"addDigitResult: {addDigitResult}");
+
+            /// Ugly Number:
+            int n = 22;
+            bool isUglyResult = Helper.IsUgly(n);
+            Console.WriteLine($"isUglyResult: {isUglyResult}");
+
+
+            /// MoveZeroes:
+            int[] nums = { 0, 1, 0, 3, 12 };
+            Helper.MoveZeroes(nums);
+            foreach (int i in nums)
+            {
+                Console.WriteLine($"element: {i}");
+            }
+
+            /// Word Pattern:
+            string pattern = "abba";
+            string str = "dog dog dog dog";
+
+            var wordPatternResult = Helper.WordPattern(pattern, str);
+            Console.WriteLine($"is pattern matched? {wordPatternResult}");
+            Console.WriteLine($"is 29 prime? {Helper.IsPrime(29)}");
+
+        }
+    }
+}
